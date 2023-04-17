b14cf17ccdcb6eaadefcbf301ee4516a7fba1f05
Praveen Venkatesh <30774147+praveenVnktsh@users.noreply.github.com>
2023-04-17 14:49:24 -0400

    added autonomous mode change

--- lib/screens/home_screen/home_call_display.dart
+++ lib/screens/home_screen/home_call_display.dart
@@ -3,6 +3,7 @@ import 'package:frontend/agora/agora_calling.dart';
 import 'package:frontend/services/teleop_services.dart';
 import 'package:frontend/widgets/joypad.dart';
 import 'package:frontend/widgets/joystick.dart';
+import 'package:firebase_database/firebase_database.dart';
 
 
 class HomeDisplay extends StatefulWidget {
@@ -14,7 +15,23 @@ class HomeDisplay extends StatefulWidget {
 
 class _HomeDisplayState extends State<HomeDisplay> {
   bool curstate = false; //
+  bool isAutonomous = false;
   
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    FirebaseDatabase.instance.ref().child("operation_mode").get().then((value)  
+    {
+      
+      setState(() {
+        isAutonomous = (value.value == "AUTONOMOUS");
+        print("GOT VALUE FROM FIREBASE = " + value.value.toString());
+      });
+    });
+    
+  }
+
   @override
   Widget build(BuildContext context) {
     return Expanded(
@@ -23,30 +40,39 @@ class _HomeDisplayState extends State<HomeDisplay> {
           Container(child: JoinChannelVideo(), color: Colors.black,),
           Align(
             alignment: Alignment.topCenter,
-            child: Row(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                FloatingActionButton(
-                    elevation: 0, onPressed: () {}, child: Icon(Icons.mic)),
-                SizedBox(
-                  width: 20,
-                ),
-                FloatingActionButton(
-                    elevation: 0,
-                    onPressed: () {},
-                    child: Icon(Icons.auto_graph_outlined)),
-                SizedBox(
-                  width: 20,
-                ),
-                FloatingActionButton(
-                    elevation: 0, onPressed: () {}, child: Icon(Icons.map)),
-              ],
-            ),
+            child: FloatingActionButton(
+                      elevation: 0,
+                      onPressed: () {
+                        setState(() {
+                          isAutonomous = !isAutonomous;
+                        });
+                        FirebaseServices.switchModes(isAutonomous);
+                      },
+                      child: Text(isAutonomous? "Auto": "Teleop")),
           ),
+          // Align(
+          //   alignment: Alignment.topCenter,
+          //   child: Row(
+          //     mainAxisSize: MainAxisSize.min,
+          //     children: [
+          //       FloatingActionButton(
+          //           elevation: 0, onPressed: () {}, child: Icon(Icons.home)),
+          //       SizedBox(
+          //         width: 20,
+          //       ),
+                
+          //       // SizedBox(
+          //       //   width: 20,
+          //       // ),
+          //       // FloatingActionButton(
+          //       //     elevation: 0, onPressed: () {}, child: Icon(Icons.map)),
+          //     ],
+          //   ),
+          // ),
           Padding(
             padding: const EdgeInsets.all(32.0),
             child: Align(
-              alignment: Alignment.bottomCenter,
+              alignment: Alignment.bottomCenter,  
               child: FloatingActionButton(
                 onPressed: () {
                   setState(() {
--- lib/services/teleop_services.dart
+++ lib/services/teleop_services.dart
@@ -2,7 +2,7 @@ import 'package:firebase_database/firebase_database.dart';
 
 abstract class FirebaseServices {
   static Future<void> teleopGripperYaw(double x,) async {
-    final DatabaseReference db = FirebaseDatabase.instance.ref().child("teleop_commands/manipulator/gripper");
+    final DatabaseReference db = FirebaseDatabase.instance.ref().child("teleop_commands/manipulator/");
     final double xVal = double.parse(x.toStringAsFixed(5));
     print(db.path);
 
@@ -23,21 +23,29 @@ abstract class FirebaseServices {
     final double xVal = double.parse(x.toStringAsFixed(5));
     final double yVal = double.parse(y.toStringAsFixed(5));
     db.update({
-      "teleop_commands/manipulator/vel_lift": xVal,
-      "teleop_commands/manipulator/vel_extend": yVal,
+      "teleop_commands/manipulator/vel_lift": yVal,
+      "teleop_commands/manipulator/vel_extend": xVal,
     });
   }
   
   static Future<void> teleopGripper(bool open) async {
     final DatabaseReference db = FirebaseDatabase.instance.ref();
     db.update({
-      "teleop_commands/manipulator/gripper/gripper_open": open,
+      "teleop_commands/manipulator/gripper_open": open,
     });
   }
 
 
+  // static Future<Map<dynamic, dynamic> > getPath(String path) async{
+  //   final DatabaseReference db = FirebaseDatabase.instance.ref().child(path);
+  //   return db.get().then((value) => value.value);
+  // }
+
   static Future<void> switchModes(bool isAutonomous) async {
     final DatabaseReference db = FirebaseDatabase.instance.ref();
+    
+
+    print(isAutonomous);
     if(!isAutonomous) {
       db.update({
         "operation_mode": "TELEOPERATION",
